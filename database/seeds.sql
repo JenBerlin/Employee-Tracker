986d826fad3d332dbe76781b5d2dee109403f832
Jenny Tschauschew <kaesekuchen123@yahoo.de>
2021-12-10 20:28:56 +0100

    index.js fuction view roles employee, add department role

--- database/seeds.sql
+++ database/seeds.sql
@@ -2,11 +2,11 @@ INSERT INTO department (name)
 VALUES ("Legal"), ("Finance"), ("HR"), ("Marketing");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lawyer", 200.000, 1),
-        ("Accountant", 100.000, 2),
-        ("Key Account", 70.000, 3),
-        ("Public Relations", 70.000, 4),
-        ("Social Media", 50.000, 4);
+VALUES ("Lawyer", 200, 1),
+        ("Accountant", 100, 2),
+        ("Key Account", 70, 3),
+        ("Public Relations", 70, 4),
+        ("Social Media", 50, 4);
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Anna", "Smith", 5),
--- index.js
+++ index.js
@@ -6,33 +6,42 @@ async function start() {
   mainQuestions();
 }
 
-function mainQuestions() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "Choices",
-        message: "Please choose one of the options:",
-        choices: [
-          "View all departments",
-          "View all roles",
-          "View all employees",
-          "Add a department",
-          "Add a role",
-          "Add an employee",
-          "Update an employee role",
-          "Exit",
-        ],
-      },
-    ])
-    .then((res) => {
-      let choice = res.Choices;
-      switch (choice) {
-        case "View all departments":
-          viewDepartments();
-          break;
-      }
-    });
+async function mainQuestions() {
+  const res = await inquirer.prompt([
+    {
+      type: "list",
+      name: "Choices",
+      message: "Please choose one of the options:",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit",
+      ],
+    },
+  ]);
+  let choice = res.Choices;
+  switch (choice) {
+    case "View all departments":
+      viewDepartments();
+      break;
+    case "View all roles":
+      viewRoles();
+      break;
+    case "View all employees":
+      viewEmployees();
+      break;
+    case "Add a department":
+      addDepartment();
+      break;
+    case "Add a role":
+      addRole();
+      break;
+  }
 }
 
 function viewDepartments() {
@@ -41,6 +50,85 @@ function viewDepartments() {
     if (err) throw err;
     console.table(res);
     mainQuestions();
+  });
+}
+
+function viewRoles() {
+  let query = "SELECT * FROM role";
+  db.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    mainQuestions();
+  });
+}
+
+function viewEmployees() {
+  let query = `
+      SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary 
+      FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id`;
+  db.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    mainQuestions();
+  });
+}
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter the new departments name:",
+      },
+    ])
+    .then((res) => {
+      let name = res.name;
+      const query = `INSERT INTO department (name) VALUES ("${name}")`;
+      db.query(query, (err, res) => {
+        if (err) throw err;
+        mainQuestions();
+      });
+    });
+}
+
+function addRole() {
+  let query = "SELECT name FROM department";
+  db.query(query, (err, res) => {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Please enter the new role:",
+        },
+
+        {
+          type: "input",
+          name: "salary",
+          message: "Please enter the salary:",
+        },
+        {
+          type: "list",
+          name: "Choices",
+          message: "Please choose one of the departments:",
+          choices: res,
+        },
+      ])
+      .then((res) => {
+        //         INSERT INTO table2 (column1, column2, column3, ...)
+        // SELECT column1, column2, column3, ...
+        // FROM table1
+        // WHERE condition;
+        const query = `INSERT INTO role
+        (title, salary, department_id)
+        SELECT "${res.name}", ${res.salary}, id FROM department WHERE name = "${res.Choices}"`;
+        db.query(query, (err, res) => {
+          if (err) throw err;
+          mainQuestions();
+        });
+      });
   });
 }
 
